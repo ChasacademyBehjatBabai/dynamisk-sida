3700d753a25e247de99d813b4b3db2604279883d
ChasacademyBehjatBabai <behjat.babai@chasacademy.se>
2022-11-08 21:28:52 +0100

    fix

--- html/cv.js
+++ html/cv.js
@@ -33,42 +33,42 @@
 
 //Fetch data from local .json file
 fetch('cv.json')
-.then(function (response) {
-    return response.json();
-})
-.then(function (data) {
-    //Save .json data in 2 parts
-    let experience = data["experience"]
-    let education = data["Education"]
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        //Save .json data in 2 parts
+        let experience = data["experience"]
+        let education = data["Education"]
 
-    //Initialize string variables
-    let experienceText = ""
-    let educationText = ""
-console.log(document.getElementsByClassName(".cv-profile-text h2"))
-    //In order to print out every element in the 'experience' Array a for loop is used.
-    experienceText += document.getElementsByClassName(".cv-profile-text h2").innerHTML
-    for (let x = 0; x < experience.length; x++) {
-        experienceText += "<p><h3>" + experience[x].title + "</h3></p>";
-        experienceText += "<p>" + experience[x].year + "</p>";
-        experienceText += "<p>" + experience[x].description + "</p></br>";
-    }
-    experienceText += "</ul>"
+        //Initialize string variables
+        let experienceText = ""
+        let educationText = ""
+        console.log(document.getElementsByClassName(".cv-profile-text h2"))
+        //In order to print out every element in the 'experience' Array a for loop is used.
+        experienceText += document.getElementsByClassName(".cv-profile-text h2").innerHTML
+        for (let x = 0; x < experience.length; x++) {
+            experienceText += "<p><h3>" + experience[x].title + "</h3></p>";
+            experienceText += "<p>" + experience[x].year + "</p>";
+            experienceText += "<p>" + experience[x].description + "</p></br>";
+        }
+        experienceText += "</ul>"
 
-    //In order to print out every element in the 'education' Array a for loop is used.
-    educationText += "Education:<ul> "
-    for (let x = 0; x < education.length; x++) {
-        educationText += "<p>" + education[x].title + "</p>";
-        educationText += "<p>" + education[x].description + "</p></br>";
+        //In order to print out every element in the 'education' Array a for loop is used.
+        educationText += "Education:<ul> "
+        for (let x = 0; x < education.length; x++) {
+            educationText += "<p>" + education[x].title + "</p>";
+            educationText += "<p>" + education[x].description + "</p></br>";
 
-    }
-    educationText += "</ul>"
+        }
+        educationText += "</ul>"
 
-    //Send the text strings to the HTML page
-    document.getElementById("experience-html").innerHTML = experienceText
-    document.getElementById("education-html").innerHTML = educationText;
-})
+        //Send the text strings to the HTML page
+        document.getElementById("experience-html").innerHTML = experienceText
+        document.getElementById("education-html").innerHTML = educationText;
+    })
 
-//Logs here in case of error
-.catch(function (err) {
-    console.log(err);
-});
+    //Logs here in case of error
+    .catch(function (err) {
+        console.log(err);
+    });
